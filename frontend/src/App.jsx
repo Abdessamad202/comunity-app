31c9d929f64d928143c5dee2379bd89cbff2d7f0
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-13 17:58:14 +0100

    Updated App.jsx with main application logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router"
 import { RegisterForm } from "./components/Auth/RegisterForm"
 import { VerificationForm } from "./components/Auth/VerificationForm"
 import { LoginForm } from "./components/Auth/LoginForm"
+import {  ProfileCompletionForm } from "./components/Auth/Profile"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/register" element={<RegisterForm />} />
       <Route path="/verify" element={<VerificationForm />} />
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/profile" element={<ProfileCompletionForm />} />
     </Routes>
   )
 }
