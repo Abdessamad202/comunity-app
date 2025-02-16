00cfbf356ce7669789922124b5de832725ab3c5f
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-16 01:23:54 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+      <Provider store={store}>
+        <App />
+
+      </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>
 )
