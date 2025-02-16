79bf2814d3efeefb42f25ba8123347c8f20cad42
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-02-16 01:23:53 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,8 @@
-import { Route, Routes } from "react-router"
-import { RegisterForm } from "./components/Auth/RegisterForm"
-import { VerificationForm } from "./components/Auth/VerificationForm"
-import { LoginForm } from "./components/Auth/LoginForm"
-import {  ProfileCompletionForm } from "./components/Auth/Profile"
+import { AppRoutes } from "./routes"
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/verify" element={<VerificationForm />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/profile" element={<ProfileCompletionForm />} />
-    </Routes>
+    <AppRoutes />
   )
 }
 
